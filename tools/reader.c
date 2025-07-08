cacf771f1c7c6081acdd4f7d6a1d985a4750d270
Sekewa <killian.piel06@gmail.com>
2025-07-08 15:23:25 +0200

    ajout d'une structure pour contenir les informations du fichier

--- tools/reader.c
+++ tools/reader.c
@@ -1,5 +1,12 @@
 #include "../includes/reader.h"
 
+typedef struct {
+    char magicNumber[2];
+    char fileSize[4];
+    char appWhoCreatedFile[4];
+    char offset[4];
+}HEADER;
+
 FILE* getFile(char* fileName){
     FILE* fileptr = NULL;
 
@@ -8,6 +15,8 @@ FILE* getFile(char* fileName){
     if(fileptr == NULL) {
         fprintf(stderr,"Error : the file don't exist or another error occured");
     }
+
+
 
     fclose(fileptr);
 
