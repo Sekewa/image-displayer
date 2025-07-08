d87e2dba4c5f043fc5c24d327dc3c9a4bc20d2e9
Sekawa <killian.piel06@gmail.com>
2025-07-08 20:20:14 +0200

    Ajout de la lecture jusqu'au donnée

--- includes/reader.h
+++ includes/reader.h
@@ -3,7 +3,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-FILE* getFile(char* fileName);
+typedef struct {
+    uint8_t magicNumber[2];
+    uint32_t fileSize;
+    uint32_t offset;
+    FILE* fileptr;
+}BMP_FILE;
+
+BMP_FILE* getFile(char* fileName);
 
 #endif // READER_C_INCLUDED
--- tools/reader.c
+++ tools/reader.c
@@ -1,13 +1,6 @@
 #include "../includes/reader.h"
 
-typedef struct {
-    char magicNumber[2];
-    char fileSize[4];
-    char appWhoCreatedFile[4];
-    char offset[4];
-}HEADER;
-
-FILE* getFile(char* fileName){
+BMP_FILE* getFile(char* fileName){
     FILE* fileptr = NULL;
 
     fileptr = fopen(fileName,"r");
@@ -16,8 +9,32 @@ FILE* getFile(char* fileName){
         fprintf(stderr,"Error : the file don't exist or another error occured");
     }
 
+    BMP_FILE* file = calloc(1,sizeof(BMP_FILE));
 
+    file->fileptr = fileptr;
 
-    fclose(fileptr);
+    file->magicNumber[0] = fgetc(file->fileptr);
+    file->magicNumber[1] = fgetc(file->fileptr);
 
+    char* sizeFile = malloc(sizeof(char) * 4);
+
+    file->fileSize += fgetc(file->fileptr);
+    file->fileSize += fgetc(file->fileptr);
+    file->fileSize += fgetc(file->fileptr);
+    file->fileSize += fgetc(file->fileptr);
+
+    // move to the start of BMP data
+    fseek(file->fileptr,4,SEEK_CUR);
+
+    file->offset += fgetc(file->fileptr);
+    file->offset += fgetc(file->fileptr);
+    file->offset += fgetc(file->fileptr);
+    file->offset += fgetc(file->fileptr);
+
+    //fprintf(stdout,"Start Addr : %x\n",file->offset);
+
+    // moving to the start of data, once there the cursor is set to use data
+    fseek(file->fileptr,file->offset,SEEK_SET);
+
+    return file;
 }
